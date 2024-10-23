d774f991017a43098ef6e5b14b6e16d07c386f40
D1dii <didacgp123@gmail.com>
2024-10-23 09:09:08 +0200

    Player Control fixed

--- Arkanoid/Assets/Scripts/PlayerController.cs
+++ Arkanoid/Assets/Scripts/PlayerController.cs
@@ -9,11 +9,15 @@ public class PlayerController : MonoBehaviour
     public Slider sliderDis;
     public bool isAutomatic = false;
     public GameObject ball;
+    public GameObject leftLimit;
+    public GameObject rightLimit;
+   
 
     // Start is called before the first frame update
     void Start()
     {
         
+       
     }
 
     // Update is called once per frame
@@ -26,7 +30,7 @@ public class PlayerController : MonoBehaviour
         }
         else if (!isAutomatic)
         {
-            transform.position = new Vector3(Mathf.Lerp(233.55f, 859.67f, sliderDis.value), transform.position.y, transform.position.z);
+            transform.position = new Vector3(Mathf.Lerp(leftLimit.transform.position.x, rightLimit.transform.position.x, sliderDis.value), transform.position.y, transform.position.z);
         }
 
         if(Input.GetKeyDown(KeyCode.O))
